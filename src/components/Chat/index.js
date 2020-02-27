ea050e87faea14caa2ad2184880029f92d73a04f
alexisOclock <alexis@oclock.io>
2020-02-27 10:16:39 +0100

    on place la data qui évoluera dans le temps en fonction des interactions dans le state :boom: les composants ne reçoivent plus de data il faut rebrancher le state avec les composants via les props

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,24 +3,6 @@ import React from 'react';
 import Messages from 'src/components/Messages';
 import Form from 'src/components/Form';
 
-const initialMessages = [
-  {
-    id: 1,
-    author: 'Super chat',
-    content: 'Salut',
-  },
-  {
-    id: 2,
-    author: 'Toto',
-    content: 'Ça va ?',
-  },
-  {
-    id: 3,
-    author: 'Super chat',
-    content: 'a+',
-  },
-];
-
 const Chat = () => (
   <div id="chatroom">
     <Messages messages={initialMessages} />
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,22 @@
-const initialState = {};
+const initialState = {
+  messages: [
+    {
+      id: 1,
+      author: 'Super chat',
+      content: 'Salut',
+    },
+    {
+      id: 2,
+      author: 'Toto',
+      content: 'Ça va ?',
+    },
+    {
+      id: 3,
+      author: 'Super chat',
+      content: 'a+',
+    },
+  ],
+};
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
